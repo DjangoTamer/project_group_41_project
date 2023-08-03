e80068f63e3b037d548309e043d8e814a9bcddf3
Slava <dyuminslava155@gmail.com>
2023-08-03 19:59:12 +0300

    les8 add admin

--- advertisements/app_advertisements/admin.py
+++ advertisements/app_advertisements/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Advertisement
 
-# Register your models here.
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'price', 'auction', 'created_at', 'created_date']
+    list_filter = ['auction', 'created_at']
+    actions = ['make_auction_false', 'make_auction_true']
+
+    @admin.action(description='Перевести auction в False')
+    def make_auction_false(self, request, query):
+        query.update(auction=False)
+
+    @admin.action(description='Перевести auction в True')
+    def make_auction_true(self, request, query):
+        query.update(auction=True)
+
+    fieldsets = (
+        ('Общее', {
+            'fields': ('title', 'description'),
+        }),
+        ('Финансы', {
+            'fields': ('price', 'auction'),
+            'classes': ['collapse']
+        })
+    )
+
+admin.site.register(Advertisement, AdvertisementAdmin)
